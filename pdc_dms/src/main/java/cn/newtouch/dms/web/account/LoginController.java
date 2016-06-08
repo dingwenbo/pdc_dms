16851b22e151130e3597390f715bbfd6b046cfdc
dingwenbo <939030515@qq.com>
2016-06-08 10:30:39 +0800

    initialize project pdc_dms.

---
+++ pdc_dms/src/main/java/cn/newtouch/dms/web/account/LoginController.java
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ * Copyright (c) 2005, 2014 springside.github.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *******************************************************************************/
+package cn.newtouch.dms.web.account;
+
+import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * LoginController负责打开登录页面(GET请求)和登录出错页面(POST请求)，
+ * 
+ * 真正登录的POST请求由Filter完成,
+ * 
+ * @author calvin
+ */
+@Controller
+@RequestMapping(value = "/login")
+public class LoginController {
+
+    Logger log = LoggerFactory.getLogger(LoginController.class);
+	@RequestMapping(method = RequestMethod.GET)
+	public String login() {
+	    log.info("ACCOUNT LOGIN");
+		return "account/login";
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public String fail(@RequestParam(FormAuthenticationFilter.DEFAULT_USERNAME_PARAM) String userName, Model model) {
+		model.addAttribute(FormAuthenticationFilter.DEFAULT_USERNAME_PARAM, userName);
+		return "account/login";
+	}
+	
+	@RequestMapping("/success")
+	public String success() {
+	    
+	    return "loginOK";
+	}
+
+}
---
+++ pdc_dms/src/main/webapp/WEB-INF/layouts/footer.jsp
@@ -0,0 +1,5 @@
+<%@ page language="java" pageEncoding="UTF-8" %>
+<div id="footer">
+	Copyright &copy; 2016-2017 <a href="http://www.newtouch.cn" target="_blank">newtouch.cn</a>
+</div>
+
