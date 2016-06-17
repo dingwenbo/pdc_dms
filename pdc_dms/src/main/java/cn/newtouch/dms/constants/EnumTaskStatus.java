a3f0cab1cd159ac1a8b65b46ef4e5bb304bec207
jialong.wang5191 <jialong.wang@newtouch.cn>
2016-06-17 16:36:44 +0800

    更新EnumTaskStatus，增加id属性。

--- pdc_dms/src/main/java/cn/newtouch/dms/constants/EnumTaskStatus.java
+++ pdc_dms/src/main/java/cn/newtouch/dms/constants/EnumTaskStatus.java
@@ -8,17 +8,31 @@ package cn.newtouch.dms.constants;
  */
 public enum EnumTaskStatus {
 	
-	TODO("TODO"),
-	IN_PROGRESSING("In Progressing"),
-	PENDING("Pending"),
-	FINISHED("Finished");
+	TODO(1, "TODO"),
+	IN_PROGRESSING(2, "In Progressing"),
+	PENDING(3, "Pending"),
+	FINISHED(4, "Finished");
 	
+	private Integer id;
 	private String code;
 	
-	private EnumTaskStatus(String code) {
+	private EnumTaskStatus(Integer id, String code) {
+		this.id = id;
 		this.code = code;
 	}
 	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
 	public String getCode() {
 		return this.code;
 	}
