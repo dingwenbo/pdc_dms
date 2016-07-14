5b0be539c53e1ca93ea7005cf3818f8c536d49b1
JiaLong Wang <rdltspg77@163.com>
2016-07-14 09:41:29 +0800

    Change to h2 db development

---
+++ pdc_dms/src/main/resources/sql/h2/02-initialize_data_basic.sql
@@ -0,0 +1,13 @@
+delete from pdc_project;
+delete from pdc_member;
+delete from pdc_role;
+
+insert into pdc_role(id, code, label, prior) values(1, 'Admin', 'Administrator of PDC-DMS', 0);
+insert into pdc_role(id, code, label, prior) values(2, 'PM', 'Project manager', 1);
+insert into pdc_role(id, code, label, prior) values(3, 'TL', 'Team Leader', 2);
+insert into pdc_role(id, code, label, prior) values(4, 'SE', 'Senior engineer', 3);
+insert into pdc_role(id, code, label, prior) values(5, 'DEV', 'Developer', 4);
+
+/* 插入超管, 密码为admin */
+insert into pdc_member(id, pdc_id, password, salt, name, role_id, register_date) 
+	values(9001, 'A000000', '691b14d79bf0fa2215f155235df5e670b64394cc', '7efbd59d9741d34f', '系统管理员', 1, now());
---
+++ pdc_dms/src/test/resources/data/h2/00-import_data_test.sql
@@ -0,0 +1,53 @@
+delete from pdc_member where role_id != 1;
+delete from pdc_project;
+delete from pdc_project_member;
+delete from pdc_task_status;
+delete from pdc_task;
+delete from pdc_log_detail;
+
+/*
+	普通测试用户的密码统一为:user
+	测试用户的id统一为9开头
+*/
+insert into pdc_member(id, pdc_id, password, salt, name, role_id, register_date)
+	values(9002, 'T000002', '2488aa0c31c624687bd9928e0a5d29e7d1ed520b', '6d65d24122c30500', '测试PM1', 2, now());
+	
+insert into pdc_member(id, pdc_id, password, salt, name, role_id, supervisor_id, register_date)
+	values(9003, 'T000003', '2488aa0c31c624687bd9928e0a5d29e7d1ed520b', '6d65d24122c30500', '测试TL1', 3, 9002, now());
+insert into pdc_member(id, pdc_id, password, salt, name, role_id, supervisor_id, register_date)
+	values(9004, 'T000004', '2488aa0c31c624687bd9928e0a5d29e7d1ed520b', '6d65d24122c30500', '测试SE1', 4, 9003, now());
+insert into pdc_member(id, pdc_id, password, salt, name, role_id, supervisor_id, register_date)
+	values(9005, 'T000005', '2488aa0c31c624687bd9928e0a5d29e7d1ed520b', '6d65d24122c30500', '测试SE2', 4, 9003, now());
+insert into pdc_member(id, pdc_id, password, salt, name, role_id, supervisor_id, register_date)
+	values(9006, 'T000006', '2488aa0c31c624687bd9928e0a5d29e7d1ed520b', '6d65d24122c30500', '测试DEV1', 5, 9004, now());
+insert into pdc_member(id, pdc_id, password, salt, name, role_id, supervisor_id, register_date)
+	values(9007, 'T000007', '2488aa0c31c624687bd9928e0a5d29e7d1ed520b', '6d65d24122c30500', '测试DEV2', 5, 9005, now());
+insert into pdc_member(id, pdc_id, password, salt, name, role_id, supervisor_id, register_date)
+	values(9008, 'T000008', '2488aa0c31c624687bd9928e0a5d29e7d1ed520b', '6d65d24122c30500', '测试DEV3', 5, 9005, now());
+insert into pdc_member(id, pdc_id, password, salt, name, role_id, supervisor_id, register_date)
+	values(9009, 'T000009', '2488aa0c31c624687bd9928e0a5d29e7d1ed520b', '6d65d24122c30500', '测试DEV4', 5, 9005, now());
+
+insert into pdc_member(id, pdc_id, password, salt, name, role_id, register_date)
+	values(9010, 'T000010', '2488aa0c31c624687bd9928e0a5d29e7d1ed520b', '6d65d24122c30500', '测试PM2', 2, now());
+insert into pdc_member(id, pdc_id, password, salt, name, role_id, supervisor_id, register_date)
+	values(9011, 'T000011', '2488aa0c31c624687bd9928e0a5d29e7d1ed520b', '6d65d24122c30500', '测试TL2', 3, 9010, now());
+
+insert into pdc_project(id, code, label) values(1, 'TEST1', 'First Test project'); 
+insert into pdc_project(id, code, label) values(2, 'TEST2', 'Second Test project');
+insert into pdc_project(id, code, label) values(3, 'TEST3', 'Third Test project');
+
+insert into pdc_project_member(project_id, member_id) values(1, 9002);
+insert into pdc_project_member(project_id, member_id) values(1, 9003);
+insert into pdc_project_member(project_id, member_id) values(2, 9010);
+
+insert into pdc_task_status(id, code) values(1, 'In progress');
+insert into pdc_task_status(id, code) values(2, 'Finished');
+insert into pdc_task_status(id, code) values(3, 'TODO');
+insert into pdc_task_status(id, code) values(4, 'Pending');
+
+insert into pdc_task(id, project_id, code, title, status_id, percent, comment) values(1, 1, 'TASK-01', 'TEST-TASK-01', 1, '20%', 'no comment');
+insert into pdc_task(id, project_id, code, title, status_id, percent, comment) values(2, 1, 'TASK-02', 'TEST-TASK-02', 2, '50%', 'comment');
+
+insert into pdc_log_detail(task_id, member_id, day, work_time, absent) values(1, 9002, now(), 0.5, null);
+insert into pdc_log_detail(task_id, member_id, day, work_time, absent) values(1, 9003, now(), 1, null);
+insert into pdc_log_detail(task_id, member_id, day, work_time, absent) values(2, 9002, now(), 0.25, null);
