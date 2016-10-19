7b76959e205953bf49a0ac5d179952b09ed00336
JiaLong Wang <rdltspg77@163.com>
2016-10-19 17:53:04 +0800

    Remove useless import.

--- pdc_dms/src/main/java/cn/newtouch/dms/web/ProfileController.java
+++ pdc_dms/src/main/java/cn/newtouch/dms/web/ProfileController.java
@@ -7,7 +7,6 @@ package cn.newtouch.dms.web;
 
 import javax.validation.Valid;
 
-import org.apache.shiro.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
